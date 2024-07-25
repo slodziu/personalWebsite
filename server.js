0f95f30fcb2c31dca6e086a9a07e3367f44ef2cc
slodziu <somlgpl@gmail.com>
2024-07-25 17:38:25 +0100

    Added prototype of experiments page

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3001;
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
